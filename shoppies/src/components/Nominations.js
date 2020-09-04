2559ff977721000aefdad8d35a5e905c46541597
Joey Nevarez <josephnevarez12@gmail.com>
2020-09-03 21:36:59 -0700

    adds react-modal, congrats banner is now a modal

--- shoppies/src/components/Nominations.js
+++ shoppies/src/components/Nominations.js
@@ -1,31 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import { removeMovie } from '../actions/index';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrophy } from '@fortawesome/free-solid-svg-icons';
 import cuid from "cuid";
-import crowd from '../sounds/crowd.wav';
-import { Howl } from 'howler';
+import Modal from 'react-modal';
 
 const Nominations = (props) => {
-    function playSound (src) {
-        const sound = new Howl({src: [crowd]})
-        sound.play()
-    }
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+
+    useEffect(() => {
+        if (props.nominatedMovies.length === 5) {
+            setModalIsOpen(true)
+        }
+    }, [props.nominatedMovies])
+
+    const customStyles = {
+        content : {
+            top: '50%',
+            left: '50%',
+            right: 'auto',
+            bottom: 'auto',
+            marginRight: '-50%',
+            transform: 'translate(-50%, -50%)',
+            width: '25%',
+            height: '20%'
+        }
+      };
+
+    Modal.setAppElement('#root')
 
     return (
         <div id="nominationsDiv">
             <h2>Nominations</h2>
-            
-            {props.nominatedMovies.length >= 5 ? (
-                <div id="banner">
-                    playSound()
-                    <FontAwesomeIcon class="trophy" icon={faTrophy}/>
-                    <p>Congratulations!<br/> The nominations are in!</p>
-                </div>
-            ): (
-                null
-            )}
+
+            <Modal
+                isOpen={modalIsOpen}
+                onRequestClose={()=>setModalIsOpen(false)}
+                style={customStyles}
+                contentLabel="Congrats Banner">
+                    <div id="banner">
+                        <FontAwesomeIcon class="trophy" icon={faTrophy}/>
+                        <p>Congratulations!<br/> The nominations are in!</p>
+                    </div>
+            </Modal>
 
             <ul>
                 {props.nominatedMovies && props.nominatedMovies.map(movie => {
